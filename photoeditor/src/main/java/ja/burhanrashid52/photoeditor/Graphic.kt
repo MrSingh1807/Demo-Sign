f304a8d761736048e488404a33345500e652eea1
MrSingh1807 <mayank.mytracker@gmail.com>
2023-11-25 00:15:27 +0530

    final push

--- photoeditor/src/main/java/ja/burhanrashid52/photoeditor/Graphic.kt
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/Graphic.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
-import android.widget.Toast
 import ja.burhanrashid52.photoeditor.MultiTouchListener.OnGestureControl
 import ja.burhanrashid52.photoeditor.utils.CustomSingleTouchListener
 
